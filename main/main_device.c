40decb2ffebba49c26b2592df1e2ff4b39307ed4
ELopes <overewer@gmail.com>
2024-01-11 07:58:36 -0300

    Evolving provisioning

--- main/main_device.c
+++ main/main_device.c
@@ -1,13 +1,25 @@
 
 #include <stdio.h>
 
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+#include <freertos/event_groups.h>
+
+#include <esp_log.h>
+#include <esp_wifi.h>
+#include <esp_event.h>
+#include <nvs_flash.h>
 
 #include <wifi_provisioning/manager.h>
 #include <wifi_provisioning/scheme_softap.h>
 
+
+#define PROV_TRANSPORT_SOFTAP   "softap"
+
 #include "SStruct.h"
+
+const int WIFI_CONNECTED_EVENT = BIT0;
+static EventGroupHandle_t wifi_event_group;
 
 static const char *TAG = "MAIN";
 
