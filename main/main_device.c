ac6415211b5280600105df6d1defd01105302784
ELopes <overewer@gmail.com>
2024-01-14 09:20:31 -0300

    added time and minor changes

--- main/main_device.c
+++ main/main_device.c
@@ -39,18 +39,43 @@ void *callback()
 
 void app_main(void)
 {
-    //Create Every Struct
-    Sensor Teste = sensor_init("SupaSens","Sensor de Teste","Teste",300,number,callback);
+    /************************
+        Create Every Struct
+    ************************/
 
-    //Provision WIFI
+    Sensor Teste = sensor_init("SupaSens","Sensor de Teste","Teste",300,number,callback);
+    Device B_Teste = device_init("ID0001","Main Device","Teste","Um device de testes","teste@teste.com","0123456789");
+    device_add_sensor(B_Teste,Teste);
+
+    /************************
+        Provision WIFI
+    ************************/
+
     saiot_provision_start();
 
     //TODO PROVISION CREDENTIALS TOO
+    
+    /************************
+            Get Time
+    ************************/
+    clock_start()
+    
+    //const char *tempoatual = clock_get_time();
+    
+    /************************
+        Connect to MQTT
+    ************************/
 
-    //Connect to MQTT
+    /************************
+        Connect to SAIOT
+    ************************/
 
-    //Run Every Struct
+    /************************
+        Run Every Struct
+    ************************/
 
+    //device_run(B_Teste);
+    
     while (1) {
          ESP_LOGI(TAG, "Hello World!");
          vTaskDelay(1000 / portTICK_PERIOD_MS);
