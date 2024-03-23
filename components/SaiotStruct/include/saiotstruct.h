a7e713fb85b99f7c43c3412809cdb53746c050c5
Ewerton Lopes <ewerton.va.lopes@gmail.com>
2024-03-23 10:14:24 -0300

    Base of struct

--- components/SaiotStruct/include/saiotstruct.h
+++ components/SaiotStruct/include/saiotstruct.h
@@ -7,13 +7,13 @@ extern "C" {
 
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "freertos/semphr.h"
 
 #include <stdlib.h>
 #include <stdio.h>
-#include "freertos/semphr.h"
 
-typedef void* (*sensorCallback)(void);
-typedef void  (*ActuatorCallback)(void*);
+// typedef void* (*sensorCallback)(void);
+// typedef void  (*ActuatorCallback)(void*);
 
 
 enum dispType {
@@ -26,10 +26,10 @@ enum sensorType {
     sensor_matrix
 };
 
-enum ActuatorType {
-    act_number,
-    act_matrix
-};
+// enum ActuatorType {
+//     act_number,
+//     act_matrix
+// };
 
 typedef struct {
     const char *fId;
@@ -39,25 +39,20 @@ typedef struct {
     enum sensorType internal_type;
     enum dispType disp;
 
-    // esp_mqtt_client_handle_t mqttclient;
-    
     int timeout;
 
     void *data;
-
-    sensorCallback callback;
 } Sen, *Sensor;
 
-typedef struct {
-    const char *Id;
-    const char *Name;
-    const char *type;
+// typedef struct {
+//     const char *Id;
+//     const char *Name;
+//     const char *type;
 
-    enum ActuatorType internal_type;
-    enum dispType disp;
+//     enum ActuatorType internal_type;
+//     enum dispType disp;
 
-    ActuatorCallback callback;
-} Act, *Actuator;
+// } Act, *Actuator;
 
 typedef struct {
     const char *Id;
@@ -71,20 +66,14 @@ typedef struct {
 
     int dispnumb;
 
-    // esp_mqtt_client_handle_t mqttclient;
     void *(*Disps);
 } Dev, *Device;
 
-void sensor_print(Sensor sens);
-void base_sensor_task(void * pvParams);
 Sensor sensor_init( const char      *Id           ,
                     const char      *Name         ,
                     const char      *type         ,
                     int             timeout       , 
                     enum sensorType internal_type);
-void sensor_change_data(Sensor sens,void *data);
-void sensor_run(Sensor sens);
-void sensor_end(Sensor sens);
 
 Device device_init( const char      *Id           ,
                     const char      *Name         ,
@@ -93,7 +82,6 @@ Device device_init( const char      *Id           ,
                     const char      *Login        ,
                     const char      *Password     );
 void device_add_sensor(Device devi,Sensor sens);
-void device_run(Device devi);
 
 #ifdef __cplusplus
 }
