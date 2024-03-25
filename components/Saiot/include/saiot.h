7f454db12fc4758397d01cdc45b7c8fadc969a5d
Ewerton Lopes <ewerton.va.lopes@gmail.com>
2024-03-25 07:30:20 -0300

    pre merge

--- components/Saiot/include/saiot.h
+++ components/Saiot/include/saiot.h
@@ -5,6 +5,33 @@
 extern "C" {
 #endif
 
+/********************
+ ****  Estrutura ****
+ ********************/
+
+#include "saiotstruct.h"
+
+/********************
+ ***** Conexão ******
+ ********************/
+
+#include "saiotwifi.h"
+#include "saiotmqtt.h"
+
+/********************
+ ****** Extras ******
+ ********************/
+
+#include "saiotjson.h"
+#include "saiotspiffs.h"
+
+extern Device Saiot_Device;
+
+esp_err_t saiot_init(const char      *Email        ,
+                     const char      *Senha        ,
+                     const char      *Name         ,
+                     const char      *Classe       ,
+                     const char      *Description  ){};
 
 #ifdef __cplusplus
 }
--- components/Saiot/saiot.c
+++ components/Saiot/saiot.c
@@ -1,3 +1,13 @@
 #include "saiot.h"
 
 static const char *TAG_SAIOT = "MODULE_SAIOT";
+
+Device Saiot_Device = NULL;
+
+esp_err_t saiot_init(const char      *Email        ,
+                     const char      *Senha        ,
+                     const char      *Name         ,
+                     const char      *Classe       ,
+                     const char      *Description  ){
+
+}
